bef4bfadb964be8cc96f0916080b977f51f61bb5
tandavala <jose.tandavala@gmail.com>
2018-12-21 06:26:56 +0100

    criando o modele de membros nao activos

---
+++ database/migrations/2018_12_21_052625_create_doacao_nao_activos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoacaoNaoActivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doacao_nao_activos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doacao_nao_activos');
+    }
+}
