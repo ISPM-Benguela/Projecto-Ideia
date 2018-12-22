f5dd3b7dc27e5cfdaac781ee7a22080fb558051a
tandavala <jose.tandavala@gmail.com>
2018-12-22 22:58:22 +0100

    criando o controlador para doacao de membros activos

--- app/Http/Controllers/Membro/DoacaoMembroActivo.php
+++ app/Http/Controllers/Membro/DoacaoMembroActivo.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Membro;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class Doacao extends Controller
+class DoacaoMembroActivo extends Controller
 {
     /**
      * Display a listing of the resource.
