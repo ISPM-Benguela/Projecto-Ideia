075e537346eefedf5fb518a7aa0ab4e86df6d4a2
tandavala <jose.tandavala@gmail.com>
2018-12-22 23:04:40 +0100

    refactorando o nome do controlador paadoacao de membros activos

--- app/Http/Controllers/Membro/DoacaoMembroActivoController.php
+++ app/Http/Controllers/Membro/DoacaoMembroActivoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Membro;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class DoacaoMembroActivo extends Controller
+class DoacaoMembroActivoController extends Controller
 {
     /**
      * Display a listing of the resource.
