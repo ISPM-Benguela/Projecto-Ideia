ec86735e970e7951f283d0dd1564fd16c399e896
tandavala <jose.tandavala@gmail.com>
2018-12-23 19:19:16 +0100

    migrando definitivamente o banco de dados

--- database/migrations/2018_12_23_180915_create_membro_activos_table.php
+++ database/migrations/2018_12_23_180915_create_membro_activos_table.php
@@ -15,7 +15,12 @@ class CreateMembroActivosTable extends Migration
     {
         Schema::create('membro_activos', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('talao');
+            $table->string('valor');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
