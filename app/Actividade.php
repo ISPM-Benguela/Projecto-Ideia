c0a052b8eeff3c4f2c2aec25373decc6104b7f4a
tandavala <jose.tandavala@gmail.com>
2018-12-24 07:21:00 +0100

    migrando a tabela actividades

--- app/Actividade.php
+++ app/Actividade.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actividade extends Model
 {
-    //
+    protected $fillable = [
+        'titulo',
+        'imagem',
+        'local',
+        'data_inicio',
+        'data_termino',
+    ];
 }
--- app/Http/Controllers/Membro/ActividadeController.php
+++ app/Http/Controllers/Membro/ActividadeController.php
@@ -130,3 +130,4 @@ class ActividadeController extends Controller
         //
     }
 }
+
