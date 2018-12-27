159619d6b3d5a4e9ada92b73e98153d8ea74d81c
tandavala <jose.tandavala@gmail.com>
2018-12-27 09:17:18 +0100

    criando o controlador configuracao

---
+++ app/Http/Controllers/Membro/ConfiguracaoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Membro;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ConfiguracaoController extends Controller
+{
+    //
+}
